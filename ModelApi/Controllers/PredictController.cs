26a4637d44da6b164e64a339d6b0f03607e07e9e
Marlies Kolbeek <126609832+MEKolbeek@users.noreply.github.com>
2023-06-16 12:26:42 +0200

    Add files via upload

---
+++ ModelApi/Controllers/PredictController.cs
@@ -0,0 +1,42 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.ML;
+using Newtonsoft.Json;
+using System.Diagnostics;
+
+namespace ModelApi.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class PredictController : ControllerBase
+    {
+        private static string modelPath = @"C:\Users\mkolb\Documents\LightGBMmodel.zip";
+        private static MLContext mlContext = new MLContext();
+        private static ITransformer model;
+
+        static PredictController()
+        {
+            model = mlContext.Model.Load(modelPath, out _);
+        }
+
+        [HttpPost]
+        public ActionResult<string> Post([FromBody] HandGestureData data)
+        {
+            string jsonData = LogHandGestureData(data);
+            var predictionEngine = mlContext.Model.CreatePredictionEngine<HandGestureData, HandGesturePrediction>(model);
+            var prediction = predictionEngine.Predict(data);
+
+            int predictedGestureNumber = (int)prediction.Label;
+
+            Debug.WriteLine($"Voorspelde handgebaar nummer: {predictedGestureNumber}");
+
+            return Ok(new { jsonData = jsonData });
+        }
+
+        private string LogHandGestureData(HandGestureData data)
+        {
+            var jsonData = JsonConvert.SerializeObject(data, Formatting.Indented);
+            Debug.WriteLine(jsonData);
+            return jsonData;
+        }
+    }
+}
---
+++ ModelApi/HandGesturePrediction.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.ML.Data;
+
+namespace ModelApi
+{
+    public class HandGesturePrediction
+    {
+        [ColumnName("Cijfer")]
+        public int Label;
+
+        public float[] Score;
+    }
+}
